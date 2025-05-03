86966cb788202c7f74d00368511a5684034538f7
Huraim Fatima <huraima@Huraims-MacBook-Air.local>
2025-05-03 11:57:02 -0400

    firebase setup

--- backend/src/utils/firebase-admin.js
+++ backend/src/utils/firebase-admin.js
@@ -1,17 +1,8 @@
 const admin = require('firebase-admin');
-require('dotenv').config();
-
-// Initialize Firebase Admin with service account
-// You need to download the service account key from Firebase console
-// and save it as serviceAccountKey.json in the backend directory
-// or use environment variables
-const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT 
-  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
-  : require('../../serviceAccountKey.json');
+const serviceAccount = require('../serviceAccountKey.json');
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL
+  credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
